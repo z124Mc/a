525190e13de6e22be1bb118e6c4936892e9ac183
Zak <82386861+z124Mc@users.noreply.github.com>
2021-05-26 17:46:37 +0100

    Add files via upload

---
+++ assets/languages/lang_en.php
@@ -0,0 +1,106 @@
+<?php
+// Sidebar.inc.php
+define("SIDEBAR_WELCOME", "Welcome");
+define("SIDEBAR_OPENTICKET", "Open tickets");
+define("SIDEBAR_MYTICKETS", "My tickets");
+define("SIDEBAR_ACCSETTINGS", "Accountsettings");
+define("SIDEBAR_LOGOUT", "Logout");
+define("SIDEBAR_WELCOME_GUEST", "Welcome Guest");
+define("SIDEBAR_WELCOME_GUEST_TEXT", "Please login to use the ticket system.");
+define("LOGIN", "Login");
+// Index.php
+define("INDEX_HEADING", "Home");
+define("INDEX", "Here can you create and manage your tickets.
+To use a the ticket system is a account required.
+You can create a account on the minecraft server.");
+define("INDEX_BTN", "Create a new ticket");
+// Admin.php
+define("CAT_DEL", "This category was successfully deleted.");
+define("FAQ_DEL", "This faq entry was successfully deleted.");
+define("ERROR", "An error has occurred.");
+define("CATEGORYS", "Categorys");
+define("ACTION", "Action");
+define("QUESTION", "Question");
+define("ANSWER", "Answer");
+define("NOFAQ", "Currently are no FAQ entrys exists.");
+define("USERNAME", "Username");
+define("RANK", "Rank");
+define("MEMBER", "Member");
+define("SETTINGS", "Settings");
+define("DISABLED", "Disabled");
+define("ENABLED", "Enabled");
+define("CAPTCHA_HINT", 'Create this keys for free on: <a href="https://www.google.com/recaptcha/admin">Google ReCaptcha</a> (Only needed if captcha enabled!)');
+define("SAVE", "Save");
+define("SAVED", "Your changes was saved.");
+define("CAPTCHA_PUBLIC_KEY", "Your website key");
+define("CAPTCHA_PRIVATE_KEY", "Your secret key");
+define("LANGUAGE", "Language");
+define("MC_REGISTER", "Minecraft Register");
+define("MC_REGISTER_DESC", "When you enable this function you need the Minecraft plugin that users can create a new account.");
+//Addfaq.php
+define("FAQ_CREATE_HEADING", "Create FAQ entry");
+define("ADD", "Add");
+//Addcategory.php
+define("CAT_CREATE_HEADING", "Create new category");
+define("CATEGORY", "Category");
+//Createticket.php
+define("TICKET_CREATE_HEADING", "Create a ticket");
+define("SEND", "Send");
+define("SUBJECT", "Subject");
+define("MESSAGE", "Your message");
+define("CAPTCHA_FAIL", "You have to fill in the captcha.");
+//Mytickets.php
+define("TITLE", "Title");
+define("CREATED_AT", "Created at");
+define("LAST_ANSWER_AT", "Last answer at");
+define("NONE", "None");
+define("OPEN", "Open");
+define("CLOSED", "Closed");
+define("NO_TICKET", "You don't have currently created a ticket.");
+define("MY_TICKETS", "My tickets");
+//team.php
+define("OPEN_TICKETS", "Open tickets");
+define("ALL_TICKETS", "All tickets");
+define("NO_OPEN", "Currently are no tickets open.");
+//ticket.php
+define("CREATED_BY", "Created by");
+define("CLOSE_BTN", "Close ticket");
+define("TICKET_CLOSED", "This ticket is closed!");
+define("TICKET_ERROR", "No ticket was requested.");
+define("TICKET_POSTED", "Your answer was posted.");
+define("TICKET_POSTED_ERR", "The ticket is closed. You can't write a answer to closed tickets.");
+define("TICKET_CLOSED_SUCCESS", "The ticket was closed.");
+define("TICKET_ALREADY_CLOSED", "The ticket is already closed.");
+define("TICKET_ANSWER_HEADING", "Answer to this ticket");
+define("POST", "Post");
+//settings.php
+define("PW_ERR", "Your new password is not the same. Please check it again.");
+define("PW_FORM", "New password");
+define("PW_FORM_2", "New password again");
+define("LASTLOGIN", "Last login");
+define("FIRSTLOGIN", "First login");
+//login.php
+define("PASSWORD", "Password");
+define("PASSWORD_AGAIN", "Password again");
+define("LOGIN_ERR", "Login failed. Please check your entered username and password.");
+define("LOGIN_BTN", "Create a new account");
+define("LOGIN_BTN_DESC", "<h3>How I can create a account?</h3>
+<p>Use the command <strong>/ticket createacc Email Password</strong> on the Minecraft server to create a command.</p>");
+//register.php
+define("REGISTER", "Register");
+define("REGISTER_USER_ERR", "Sorry but this username is taken");
+define("REGISTER_EMAIL_ERR", "Sorry but this email is taken");
+define("REGISTER_PW_ERR", "Sorry but your passwords do not match");
+define("REGISTER_OK", "Your account was successfully created");
+define("DISABLED_HEADER", "Error");
+define("DISABLED_MESSAGE", "The manual registration was disabled by an admin. Please use the command at the Minecraft server to create an account.");
+//editaccount.php
+define("SAVED_CHANGE", "Your changes was successfully saved.");
+define("EDIT_NO_PERMS", "You can't edit this user.");
+define("EDIT_NOT_YOU", "You can't edit yourself.");
+define("NO_REQUEST", "No user was requested.");
+//search.php
+define("SEARCH", "Search");
+define("SEARCH_KEY", "Keyword");
+define("NO_SEARCH_RESULT", "No search results were found.");
+ ?>
